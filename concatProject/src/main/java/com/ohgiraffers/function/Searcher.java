144be4971f7f35a22ba548deafcefc7732360792
yzzzang <yz1211ch@gmail.com>
2024-08-29 17:38:19 +0900

    test commit

--- concatProject/src/main/java/com/ohgiraffers/function/Searcher.java
+++ concatProject/src/main/java/com/ohgiraffers/function/Searcher.java
@@ -1,2 +1,12 @@
-package com.ohgiraffers.function;public class Searcher {
+package com.ohgiraffers.function;
+
+public class Searcher
+{
+    //이름, 전화번호, 이메일, 주소 등을 기준으로 연락처를 검색할 수 있어야 한다.
+    //검색 결과는 리스트 형태로 표시되며, 검색 조건에 따라 필터링할 수 있다.
+
+    public void search()
+    {
+
+    }
 }
