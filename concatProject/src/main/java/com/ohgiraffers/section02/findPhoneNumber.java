25dddb622cafe139c0ea0cb1af338e63bac7dd5f
zxczx3020 <zxczx3020@gmail.com>
2024-08-29 17:36:07 +0900

    no message

---
+++ concatProject/src/main/java/com/ohgiraffers/section02/findPhoneNumber.java
@@ -0,0 +1,23 @@
+package com.ohgiraffers.section02;
+
+
+import java.util.List;
+import java.util.Map;
+
+public class findPhoneNumber {
+
+    /*      - **연락처 목록 조회**
+            - 저장된 모든 연락처를 목록 형태로 조회할 수 있어야 한다. //리스트
+            !-- 목록은 이름, 전화번호, 생일, 그룹을 기준으로 정렬할 수 있다.
+            - 연락처는 그룹별로 조회할 수 있는 기능을 제공한다.
+     */
+
+  
+
+
+
+
+
+
+
+}
