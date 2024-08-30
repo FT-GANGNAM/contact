47af304a2952ef228c62313284b7d7fd828cfb7e
YSJ <dbstjrwnd123@gmail.com>
2024-08-30 18:07:59 +0900

    커밋

--- concatProject/src/main/java/com/ohgiraffers/section02/controller/ContactController_YSJ.java
+++ concatProject/src/main/java/com/ohgiraffers/section02/controller/ContactController_YSJ.java
@@ -4,6 +4,8 @@ import com.ohgiraffers.section02.dao.ContactDAO_YSJ;
 import com.ohgiraffers.section02.dto.ContactDTO_YSJ;
 
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import static com.ohgiraffers.common.JDBCTemplate.*;
 
@@ -21,19 +23,38 @@ public void insertcontact(){  //contact_name,phonenumber, email, address, birthd
     System.out.println("연락처에 추가하고 싶은 이름을 입력 해주세요: ");
     contactDTO.contact_name(scr.nextLine());
     System.out.println("연락처에 추가할 전화번호를 입력 해주세요 ('-'포함) : ");
-    contactDTO.phonenumber(scr.nextLine());
+    String phone = scr.nextLine();
+    if (isValidPhoneNumber(phone)){
+        contactDTO.phonenumber(phone);
+    }else {
+        System.out.println("입력하신 형식이 맞지 않습니다! ");
+        return ;
+    }
     System.out.println("연락처에 추가할 이메일을 입력 해주세요 : ");
-    contactDTO.email(scr.nextLine());
+    String email = scr.nextLine();
+    if (isValidEmail(email)){
+        contactDTO.email(email);
+    }else {
+        System.out.println("입력하신 이메일 형식이 맞지 않습니다");
+        return;
+    }
     System.out.println("연락처에 추가할 주소를 입력 해주세요 : ");
     contactDTO.address(scr.nextLine());
     System.out.println("연락처에 추가할 생일을 입력 해주세요 : ");
-    contactDTO.birthday(scr.nextLine());
+    String birth = scr.nextLine();
+    if(isValidBirthday(birth)){
+        contactDTO.birthday(birth);
+    }else {
+        System.out.println("입력하신 형식이 맞지 않습니다!");
+        return;
+    }
     System.out.println("연락처에 추가할 그룹번호를 입력 해주세요 : ");
     contactDTO.groupnumber(scr.nextInt());
     System.out.println("연락처에 추가할 유저코드를 입력 해주세요 : ");
-    contactDTO.usercode(scr.nextInt());
+    contactDTO.user_code(scr.nextInt());
 
     int result = contactDAO.insertcontact(getConnection(), contactDTO);
+
 
 }
 
@@ -50,10 +71,22 @@ public void updatecontact() {
     contactDTO.contact_name(scr.nextLine());
 
     System.out.println("연락처의 번호를 어떻게 바꾸시겠습니까? :  ");
-    contactDTO.phonenumber(scr.nextLine());
+        String phone = scr.nextLine();
+    if (isValidPhoneNumber(phone)){
+        contactDTO.phonenumber(phone);
+    }else {
+        System.out.println("입력하신 형식이 맞지 않습니다! ");
+        return ;
+    }
 
     System.out.println("연락처의 이메일을 어떻게 바꾸시겠습니까? : ");
-    contactDTO.email(scr.nextLine());
+    String email = scr.nextLine();
+    if (isValidEmail(email)){
+        contactDTO.email(email);
+    }else {
+        System.out.println("입력하신 이메일 형식이 맞지 않습니다");
+        return;
+    }
 
     System.out.println("연락처의 주소를 어떻게 바꾸시겠습니까? ");
     contactDTO.address(scr.nextLine());
@@ -65,7 +98,7 @@ public void updatecontact() {
     contactDTO.groupnumber(scr.nextInt());
 
     System.out.println("연락처의 유저코드를 어떻게 바꾸시겠습니까? ");
-    contactDTO.usercode(scr.nextInt());
+    contactDTO.user_code(scr.nextInt());
 
     scr.nextLine();
 
@@ -82,6 +115,29 @@ public void updatecontact() {
 
         int result = contactDAO.deletecontact(getConnection(), contactDTO);
 
+    }
+
+    public static boolean isValidPhoneNumber(String phoneNumber) {
+        String phoneRegex = "^\\d{3}-\\d{4}-\\d{4}$";
+        Pattern phonePattern = Pattern.compile(phoneRegex);
+        Matcher matcher = phonePattern.matcher(phoneNumber);
+        return matcher.matches();
+    }
+
+    // 이메일 유효성 검사
+    public static boolean isValidEmail(String email) {
+        String emailRegex = "^[\\w.-]+@[\\w.-]+\\.[a-zA-Z]{2,}$";
+        Pattern emailPattern = Pattern.compile(emailRegex);
+        Matcher matcher = emailPattern.matcher(email);
+        return matcher.matches();
+    }
+
+    // 생일 유효성 검사
+    public static boolean isValidBirthday(String birthday) {
+        String birthdayRegex = "^(0[1-9]|1[0-2])월 (0[1-9]|[12][0-9]|3[01])일$"; // ex) 08월 13일
+        Pattern birthdayPattern = Pattern.compile(birthdayRegex);
+        Matcher matcher = birthdayPattern.matcher(birthday);
+        return matcher.matches();
     }
 
 
