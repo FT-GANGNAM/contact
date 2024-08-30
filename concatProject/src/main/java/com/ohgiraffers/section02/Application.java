1347ed67b19883874a1881602afe1da90b98b965
temless <ljw980722@naver.com>
2024-08-30 09:58:38 +0900

    커밋

--- concatProject/src/main/java/com/ohgiraffers/section02/Application.java
+++ concatProject/src/main/java/com/ohgiraffers/section02/Application.java
@@ -1,8 +1,15 @@
 package com.ohgiraffers.section02;
 
+import com.ohgiraffers.section01.controller.ContactController;
+
 public class Application {
     public static void main(String[] args) {
 
+        ContactController c = new ContactController();
+        c.totalCount();
+
+
+        c.groupByPhoneNumber();
 
     }//main
 }//class
