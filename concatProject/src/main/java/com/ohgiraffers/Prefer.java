ee5c122642f31e8c2b1d5b6115249bc71caa2adc
YSJ <dbstjrwnd123@gmail.com>
2024-09-03 12:21:16 +0900

    정렬방식 생성 및 xml 수정

---
+++ concatProject/src/main/java/com/ohgiraffers/Prefer.java
@@ -0,0 +1,51 @@
+package com.ohgiraffers;
+
+public enum Prefer {
+
+
+    EMAIL_ASC("email ASC",1),
+
+    EMAIL_DESC("email DESC",2),
+
+    NAME_ASC("contact_name ASC",3),
+
+    NAME_DESC("contact_name DESC",4),
+
+    BIRTHDAY_ASC("birthday ASC",5),
+
+    BIRTHDAY_DESC("birthday DESC",6);
+
+
+
+
+
+
+
+
+
+    private final String description;
+    private final int count;
+
+    public static  String description(int a) {
+            String des = null;
+        for(Prefer p : Prefer.values()) {
+            if(p.count== a){
+                des = p.getDescription();
+
+
+            }
+
+        }
+        return des;
+    }
+
+    Prefer(String description, int count) {     // 자동으로 상수의 속성(소괄호안의값)이 description으로 들어온다!
+        this.description = description;
+        this.count = count;// 즉, 인스턴스를 만들면 상수의 속성이 따라온다!
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+}
--- concatProject/src/main/java/com/ohgiraffers/section02/Controller.java
+++ concatProject/src/main/java/com/ohgiraffers/section02/Controller.java
@@ -1,14 +1,13 @@
 package com.ohgiraffers.section02;
 
+import com.ohgiraffers.Prefer;
+import com.ohgiraffers.section01.dto.ContactDTO;
 import com.ohgiraffers.section01.dto.GroupContactDTO;
-import com.ohgiraffers.section02.findPhoneNumber;
 
 import java.sql.Connection;
-import java.util.List;
-import java.util.Map;
-import java.util.Scanner;
+import java.util.*;
+import java.util.prefs.Preferences;
 
-import static com.ohgiraffers.common.JDBCTemplate.*;
 import static com.ohgiraffers.common.JDBCTemplate.getConnection;
 
 public class Controller {
@@ -25,15 +24,17 @@ public class Controller {
 
             System.out.println("1. 전체 목록 조회");
             System.out.println("2. 그룹으로 조회");
+            System.out.println("0. 초기화면으로 돌아가기");
             System.out.println("목록을 선택해주세요 : ");
             int find = scr.nextInt();
 
 
             switch (find) {
 
-                case 1 : con.test2(); break;
+                case 1 : con.test2(userCode); break;
                 case 2 : con.test();
                     break;
+                    case 0 : return;
             }
 
         }
@@ -51,13 +52,33 @@ public class Controller {
         }
     }
 
-    public void test2(){
-        List<GroupContactDTO> test2 = findphoneNumber.findPhoneNumbers1(getConnection());
-        for (GroupContactDTO groupContactDTO : test2) {
-            System.out.println(groupContactDTO);
-        }
-    }
+    public void test2(int userCode) {
+        List<ContactDTO> test1 = findphoneNumber.findPhoneNumbers1(getConnection(), userCode);
+        Scanner scr = new Scanner(System.in);
 
+        // 첫 번째 리스트 출력
+        for (ContactDTO contactDTO : test1) {
+            System.out.println(contactDTO);
+        }
+            while (true) {
+                System.out.println("어느 기준으로 정렬 하시겠습니까?");
+                System.out.println("1.이메일 내림차순 2. 이메일 오름차순 3. 이름 내림차순 4.오름차순 5. 생일별 내림차순 6. 생일별 오름차순 0.뒤로가기");
+                int count = scr.nextInt();
+                if (count == 0) {
+                    break;
+                }
+               String a= Prefer.description(count);
+              List<ContactDTO> test2 = findphoneNumber.findsort(getConnection(),userCode,a);
+
+                for (ContactDTO b : test2) {
+                    System.out.println(b);
+
+
+                }
+                
+            }
+
+    }
     public void findGroup(){
 
         Scanner scr = new Scanner(System.in);
